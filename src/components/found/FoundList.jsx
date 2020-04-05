1a2d7183f351295a82b9877c9776a94693486e87
dongdong <dongdong@dongdongui-MacBookAir.local>
2020-04-05 20:27:28 +0900

    리스트 페이지 페이징 작업중

--- src/components/found/FoundList.jsx
+++ src/components/found/FoundList.jsx
@@ -1,13 +1,13 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
-
+import store from 'store';
 import { useState, useEffect, Fragment } from 'react';
 import { CheckLogin } from '../CheckLogin';
 /* data_api */
 import axios from 'axios';
 
 import Loding from '../common/Loding';
-import FoundRemove from '../found/FoundRemove';
+//import FoundRemove from '../found/FoundRemove';
 
 import Icon from '../common/Icon';
 import { TABLE, LISTWRAP, LISTITEM } from '../common/Tag';
@@ -42,10 +42,53 @@ const FoundList = (props) => {
   const [foundlists, setFoundlists] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const [name, setName] = useState('');
+  const [inputText, setInputText] = useState('');
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: store.get('token'),
+    },
+  };
+
+  const onRemove = (id) => {
+    if (window.confirm('정말 삭제할꺼야?')) {
+      axios
+        .delete(`https://shrouded-escarpment-56668.herokuapp.com/api/stores/${id}`, config)
+        .then(function (response) {
+          const nextNames = foundlists.filter((name) => name._id !== id);
+          setFoundlists(nextNames);
+          alert('삭제되었습니다.');
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  };
+
+  const onSearch = (id) => {
+    //console.log(nextName);
+    const nextNames = foundlists.filter((item) => item.name.indexOf(id) !== -1);
+    // console.log(nextNames);
+    setFoundlists(nextNames);
+  };
+
+  const onPagging = (num) => {
+    console.log(num);
+    let data = [];
+    if (num === 1) {
+      data = foundlists.slice(0, 3);
+    } else if (num === 2) {
+      data = foundlists.slice(3, 6);
+    } else if (num === 3) {
+      data = foundlists.slice(6, 9);
+    }
+    setFoundlists(data);
+    console.log(data);
   };
 
   useEffect(() => {
@@ -54,6 +97,7 @@ const FoundList = (props) => {
       try {
         const response = await axios.get('https://shrouded-escarpment-56668.herokuapp.com/api/stores');
         setFoundlists(response.data);
+        //console.log(response.data);
       } catch (e) {
         if (e.message === 'Request failed with status code 401') {
           alert('401에러입니다.');
@@ -62,7 +106,7 @@ const FoundList = (props) => {
       setLoading(false);
     };
     fetchData();
-  }, [name]);
+  }, []);
 
   //로딩(컴포넌트로 뺄예정)
   if (loading) {
@@ -77,10 +121,12 @@ const FoundList = (props) => {
   return (
     <Fragment>
       <div className="a">
-        <input type="text" placeholder="국가명" value={name} onChange={onChangeName} />
+        <input type="text" placeholder="국가명" value={inputText} onChange={onChange} />
+        <button onClick={(e) => onSearch(inputText)}>검색</button>
       </div>
-      {name}
+
       <LISTWRAP css={FoundListstyle}>
+        {console.log(foundlists.length)}
         {foundlists.map((foundlist) => (
           <LISTITEM float="left" key={foundlist._id}>
             <TABLE align="left" row_height="30px" titlesize="24px" titlecolor="#333" subsize="16px">
@@ -114,11 +160,28 @@ const FoundList = (props) => {
               </tbody>
             </TABLE>
             <CheckLogin login>
-              <FoundRemove foundlistid={foundlist._id}>지점삭제</FoundRemove>
+              <button onClick={(e) => onRemove(foundlist._id)}>지점삭제</button>
             </CheckLogin>
           </LISTITEM>
         ))}
       </LISTWRAP>
+
+      {/* <div>
+        <button onClick={(e) => onPagging((foundlists.length / 3) * 1)}>1</button>
+        <br />
+        <button onClick={(e) => onPagging((foundlists.length / 3) * 2)}>2</button>
+        <br />
+        <button onClick={(e) => onPagging((foundlists.length / 3) * 3)}>3</button>
+        <br />
+      </div> */}
+      <div>
+        <button onClick={(e) => onPagging(1)}>1</button>
+        <br />
+        <button onClick={(e) => onPagging(2)}>2</button>
+        <br />
+        <button onClick={(e) => onPagging(3)}>3</button>
+        <br />
+      </div>
     </Fragment>
   );
 };
--- src/components/found/FoundRemove.jsx
+++ src/components/found/FoundRemove.jsx
@@ -5,22 +5,6 @@ import store from 'store';
 const FoundRemove = (props) => {
   //const [removelists, setRemovelists] = useState(null);
 
-  useEffect(() => {
-    console.log(store.get('token'));
-  }, []);
-
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: store.get('token'),
-    },
-  };
-
-  const onSubmit = (data) => {
-    console.log(data);
-    axios.delete(`https://shrouded-escarpment-56668.herokuapp.com/api/stores/${props.foundlistid}`, config);
-  };
-
   return <button onClick={onSubmit}>{props.children}</button>;
 };
 
